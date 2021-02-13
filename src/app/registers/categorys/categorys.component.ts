3c835df597ef477345a153f93ea03a7fa771f51d
Eanda <eandalined@gmail.com>
2021-02-13 18:28:32 +0700

    --- Auto-Git Commit ---

--- src/app/registers/categorys/categorys.component.ts
+++ src/app/registers/categorys/categorys.component.ts
@@ -1,4 +1,9 @@
 import {Component, OnInit} from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { HttpService } from '../../m-shares/services/http.service';
+import { PopupService } from '../../m-shares/services/popup.service';
+import { RequestModel } from '../../m-shares/models/model/request';
+import { CategoryModel } from '../../m-shares/models/model/category';
 
 @Component({
   selector: 'app-categorys',
@@ -7,10 +12,33 @@ import {Component, OnInit} from '@angular/core';
 })
 export class CategorysComponent implements OnInit {
 
-  constructor() {
+  categoryList = new Array<CategoryModel>();
+
+  constructor(
+    private service: HttpService,
+    private modalService: PopupService,
+    private dataService: DataService,
+    private titleService: Title
+  ) {
   }
 
   ngOnInit(): void {
+    this.inquiry();
+  }
+
+  inquiry(){
+    const trReq = new RequestModel();
+    const api = '/api/category/v1/list';
+    this.service.HTTPGet(api).then(resp => {
+      const response   = resp as any;
+      if (response) {
+
+        this.categoryList = response.body;
+        this.data          = response.body;
+        this.gridData      = this.categoryList;
+        this.loadingData(this.category_list);
+      }
+    });
   }
 
 }
